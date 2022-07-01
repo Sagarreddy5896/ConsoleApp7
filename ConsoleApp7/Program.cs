9e6781ef0bf8c4ba08da513f0c44a7227360cb34
Admin <Admin@DESKTOP-FCNC2RJ>
2022-07-01 11:13:54 +0530

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the value of the count");
+            int Count = Convert.ToInt32(Console.ReadLine());
+            Counter instance = Counter.Getinstance;
+
+
+            Console.WriteLine(instance.increment(Count));
+            Console.WriteLine(instance.decrement(Count));
+            Console.ReadLine();
+        }
+    }
+
+
+    public class Counter
+    {
+        private static Counter getinstance = null;
+
+        private Counter()
+        {
+        }
+
+        public static Counter Getinstance
+        {
+
+            get
+            {
+                if (getinstance == null)
+                {
+                    getinstance = new Counter();
+                }
+                return getinstance;
+            }
+        }
+        public int increment(int count)
+        {
+            count++;
+            return count;
+        }
+
+        public int decrement(int count)
+        {
+            count--;
+            return (count);
+        }
+
+    }
+
+
+
+}
